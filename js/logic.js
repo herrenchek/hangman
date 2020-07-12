8fe2b3471daffdf3f6bf49653aca1272e254926a
herrenchek <giselle.herr@icloud.com>
2020-07-11 23:56:21 -0400

    ES6 changes

--- js/logic.js
+++ js/logic.js
@@ -1,10 +1,20 @@
 // Initialize game variables
 
-var gameWords = ['aposimz', 'aquaelie', 'bebop', 'odyssey', 'sidonia'];
+const gameWords = ['aposimz', 'aquaelie', 'bebop', 'odyssey', 'sidonia'];
 
 // Returns random word from gameWords array
-var randomWord = function() {
-    return gameWords[Math.floor(Math.random() * gameWords.length)];
-}
+randomWord = () => gameWords[Math.floor(Math.random() * gameWords.length)];
 
 console.log(randomWord())
+
+// Determines if letter is in word and returns Boolean
+isCorrectGuess = (word, letter) => {
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            return true;
+        }
+        return false;
+    }
+}
+
+console.log(isCorrectGuess('adios', 'a'))
