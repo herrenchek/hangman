efbb6dccb508d2cac05b3f61f483c73a3853d16a
herrenchek <giselle.herr@icloud.com>
2020-07-12 00:41:30 -0400

    DRYecho

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,12 @@
 // Initialize game variables
-
-const gameWords = ['aposimz', 'aquaelie', 'bebop', 'jehuty', 'odyssey', 'sidonia'];
+const gameWords = [
+    'aposimz', 
+    'aquaelie', 
+    'bebop', 
+    'jehuty', 
+    'odyssey', 
+    'sidonia'
+];
 
 // Returns random word from gameWords array
 randomWord = () => gameWords[Math.floor(Math.random() * gameWords.length)];
@@ -8,14 +14,7 @@ randomWord = () => gameWords[Math.floor(Math.random() * gameWords.length)];
 console.log(randomWord())
 
 // Determines if letter is in word and returns Boolean
-isCorrectGuess = (word, letter) => {
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] === letter) {
-            return true;
-        }
-        return false;
-    }
-}
+isCorrectGuess = (word, letter) => word.indexOf(letter) >= 0;
 
 // Returns an array of underscores with equal length to that of the word
 getBlanks = word => {
@@ -24,4 +23,14 @@ getBlanks = word => {
         blanks.push('_');
     }
     return blanks;
+}
+
+// Fills blanks array in the correct locations given a letter and the word that array was built from
+fillBlanks = (word, puzzleState, letter) => {
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            puzzleState[i] = letter;
+        }
+    }
+    return puzzleState;
 }
